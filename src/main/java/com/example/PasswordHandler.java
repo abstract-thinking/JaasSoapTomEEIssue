af3eebeffc1882d49ed1efa2b606ee7f69efc079
Markus Hofmann <markus.hofmann@wibu.com>
2023-07-19 13:22:19 +0200

    Removed logging.

--- src/main/java/com/example/PasswordHandler.java
+++ src/main/java/com/example/PasswordHandler.java
@@ -1,29 +1,12 @@
 package com.example;
 
-import org.apache.wss4j.common.ext.WSPasswordCallback;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 
-/**
- * @author chs
- */
 public class PasswordHandler implements CallbackHandler {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(PasswordHandler.class);
 
     @Override
     public void handle(Callback[] callbacks) {
-        try {
-            for (Callback callback : callbacks) {
-                WSPasswordCallback wsPasswordCallback = (WSPasswordCallback) callback;
-                wsPasswordCallback.setPassword("huhu");
-            }
-        } catch (Exception e) {
-            LOGGER.warn(e.getMessage(), e);
-            throw e;
-        }
+
     }
 }
